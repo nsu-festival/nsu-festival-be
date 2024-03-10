e1f24e86859ba993421c469f4c2782e77ed5490e
dudxo <father8475@naver.com>
2024-03-10 15:34:10 +0900

    Fix: VisitorController 의존 관계 수정 1. 서비스 계층 VisitorService 구현 클래스가 아닌 추상 클래스로 의존 관계 변경

--- NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/visitor/controller/VisitorController.java
+++ NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/visitor/controller/VisitorController.java
@@ -1,5 +1,6 @@
 package com.example.nsu_festival.domain.visitor.controller;
 
+import com.example.nsu_festival.domain.visitor.service.VisitorService;
 import com.example.nsu_festival.domain.visitor.service.VisitorServiceImpl;
 import com.example.nsu_festival.global.etc.StatusResponseDto;
 import com.example.nsu_festival.domain.visitor.dto.VisitorResponseDto;
@@ -15,16 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/visit")
 public class VisitorController {
 
-    private final VisitorServiceImpl visitorServiceImpl;
+    private final VisitorService visitorService;
 
     @GetMapping("")
     public void getIp(HttpServletRequest request) {
-        visitorServiceImpl.savedVisitor(request);
+        visitorService.savedVisitor(request);
     }
 
     @GetMapping("/count")
     public ResponseEntity<StatusResponseDto> getCount() {
-        VisitorResponseDto countResponse = visitorServiceImpl.findVisitor();
+        VisitorResponseDto countResponse = visitorService.findVisitor();
         return ResponseEntity.ok(StatusResponseDto.success(countResponse));
     }
 }
