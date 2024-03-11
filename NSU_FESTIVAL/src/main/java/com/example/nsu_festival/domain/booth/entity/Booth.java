d2b7d4321f0bfd4640d287e33141c8acddc7564b
조아빈 <whdkqls122@naver.com>
2024-03-11 23:19:35 +0900

    feat : booth 엔티티 생성 booth ããì´당 좋아ìbooth 당 좋아요 업데이트 로직 구현

--- NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/booth/entity/Booth.java
+++ NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/booth/entity/Booth.java
@@ -1,4 +1,60 @@
 package com.example.nsu_festival.domain.booth.entity;
 
+import com.example.nsu_festival.domain.comment.entity.Comment;
+import com.example.nsu_festival.domain.likes.entity.BoothLiked;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "booth")
 public class Booth {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "booth_Id")
+    private Long boothId;
+
+    @NonNull
+    private String title;
+
+    @NonNull
+    private String content;
+
+    private Long countLike;
+
+    private String area;
+
+    @OneToMany(mappedBy = "booth", fetch = FetchType.LAZY)
+    @JsonManagedReference  //순환 참조 문제 해결하기 위한 어노테이션
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "booth",fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<BoothLiked> boothLiked = new ArrayList<>();
+
+    @OneToMany(mappedBy = "booth",fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<BoothCategory> boothCategories = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booth_image_id")
+    @JsonManagedReference
+    private BoothImage boothImage;
+
+    @OneToMany(mappedBy = "booth",fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<Menu> menus = new ArrayList<>();
+
+    public void updateCountLike(int countLike){
+        this.countLike = countLike;
+    }
 }
--- NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/booth/repository/BoothRepository.java
+++ NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/booth/repository/BoothRepository.java
@@ -1,4 +1,7 @@
 package com.example.nsu_festival.domain.booth.repository;
 
-public interface BoothRepository {
+import com.example.nsu_festival.domain.booth.entity.Booth;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoothRepository extends JpaRepository<Booth, Long> {
 }
