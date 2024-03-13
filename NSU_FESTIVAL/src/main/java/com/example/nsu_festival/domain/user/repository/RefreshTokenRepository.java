2427552a1ee75802aa4ecdc0b659f25701df4607
dudxo <father8475@naver.com>
2024-03-14 00:25:10 +0900

    Feat: 만료된 RefreshToken 삭제 1. 저장된 토큰 중 만료된 토큰 삭제 스케줄러 컴포넌트 등록 2. 만료된 토큰 삭제 Query 작성

--- NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/user/repository/RefreshTokenRepository.java
+++ NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/user/repository/RefreshTokenRepository.java
@@ -2,6 +2,8 @@ package com.example.nsu_festival.domain.user.repository;
 
 import com.example.nsu_festival.domain.user.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,4 +11,7 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     Optional<RefreshToken> findByUserEmail(String userEmail);
 
+    @Modifying
+    @Query("DELETE FROM RefreshToken t WHERE t.expiration < CURRENT_TIMESTAMP")
+    void deleteExpiredTokens();
 }
