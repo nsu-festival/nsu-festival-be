af3bbefbe46ad2943b0bd5947b726dd0993f9a45
조아빈 <whdkqls122@naver.com>
2024-03-17 17:03:18 +0900

    refactor:êµ교통정보 응답 dto 이름 수정 TrafficInformationDto -> TrafficInformationResponseDto

--- NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/trafficinformation/controller/TrafficInformationController.java
+++ NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/trafficinformation/controller/TrafficInformationController.java
@@ -1,16 +1,14 @@
 package com.example.nsu_festival.domain.trafficinformation.controller;
 
-import com.example.nsu_festival.domain.trafficinformation.dto.TrafficInformationDto;
+import com.example.nsu_festival.domain.trafficinformation.dto.TrafficInformationResponseDto;
 import com.example.nsu_festival.domain.trafficinformation.exception.TrafficInformationError;
 import com.example.nsu_festival.domain.trafficinformation.service.TrafficInformationService;
 import com.example.nsu_festival.global.etc.StatusResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -28,8 +26,8 @@ public class TrafficInformationController {
     @GetMapping("/trafficinformations")
     public ResponseEntity<StatusResponseDto> findTrafficInformation(){
         try{
-            List<TrafficInformationDto> trafficInformationDtoList = trafficInformationService.findTrafficInformation();
-            return ResponseEntity.ok().body(StatusResponseDto.success(trafficInformationDtoList));
+            List<TrafficInformationResponseDto> trafficInformationResponseDtoList = trafficInformationService.findTrafficInformation();
+            return ResponseEntity.ok().body(StatusResponseDto.success(trafficInformationResponseDtoList));
         } catch (NoSuchElementException e){
             return ResponseEntity.ok().body(StatusResponseDto.fail("예정된 운행정보가 없습니다."));
         } catch (RuntimeException e){
--- NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/trafficinformation/dto/TrafficInformationResponseDto.java
+++ NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/trafficinformation/dto/TrafficInformationResponseDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @Builder
-public class TrafficInformationDto {
+public class TrafficInformationResponseDto {
     private String updnLine;        // 열차 상하행 여부
     private String arrivalLocation; // 열차 실시간 도착역
     private String arrivalTime;     // 열차가 성환역까지 도착할때까지 남은 역
