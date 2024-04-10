4ce93a1456fca1fc6ae9d437838c7d29ef689308
박민서 <qkrals2475@naver.com>
2024-04-10 18:14:40 +0900

    fix : s3 연결 설정 수정

--- NSU_FESTIVAL/src/main/java/com/example/nsu_festival/NsuFestivalApplication.java
+++ NSU_FESTIVAL/src/main/java/com/example/nsu_festival/NsuFestivalApplication.java
@@ -4,15 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(
-		/*
-		로컬에서 s3 연결시 발생하는 에러 로그 제거 로직
-		 */
-		exclude = {
-				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
-		}
-)
+@SpringBootApplication
 @EnableScheduling
 public class NsuFestivalApplication {
 
