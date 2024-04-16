f105a3392697742ea5c68c5e697250c304070a8f
조아빈 <whdkqls122@naver.com>
2024-04-16 18:29:55 +0900

    fix : 일차별 좋아요 여부 api 인가처리 제거

--- NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/festival/entity/DDay.java
+++ NSU_FESTIVAL/src/main/java/com/example/nsu_festival/domain/festival/entity/DDay.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum DDay {
-    FIRST_DATE("2024-04-22"),
-    SECOND_DATE("2024-04-23"),
-    LAST_DATE("2024-04-24");
+    FIRST_DATE("2024-04-23"),
+    SECOND_DATE("2024-04-24"),
+    LAST_DATE("2024-04-25");
 
     private final String date;
 
